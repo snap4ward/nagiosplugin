9db97e92b379be3ddcd9752dae6372283791f757
Christian Kauhaus <christian@kauhaus.de>
2011-07-15 14:27:49 +0200

    Review Range class
    Re #9266

--- src/nagiosplugin/threshold.py
+++ src/nagiosplugin/threshold.py
@@ -10,6 +10,7 @@ class Threshold(object):
     """A Threshold object combines warning and critical ranges."""
 
     def __init__(self, warning=None, critical=None):
+        """Create Threshold object with `warning` and `critical` ranges."""
         if not warning and not critical:
             raise ValueError(u'Threshold needs at least one of warning and '
                              u'critical')
@@ -22,8 +23,9 @@ class Threshold(object):
     def match(self, value, crit_msg=None, warn_msg=None, ok_msg=None):
         """Return State object depending on value and ranges.
 
-        If value is outside the critical or warning ranges, the returned state
-        object is optionally constructed with the respective message string.
+        If `value` is outside the critical or warning ranges, the returned
+        state object is optionally constructed with the respective message
+        string.
         """
         if self.critical and not self.critical.match(value):
             return nagiosplugin.state.Critical()
