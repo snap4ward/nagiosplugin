3a7f4d01de3091e7c0dc2ae798feee79656385b2
Christian Kauhaus <kc@flyingcircus.io>
2015-09-03 12:58:04 +0200

    Introduce tox, drop Python 2 support

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
         'License :: OSI Approved :: Zope Public License',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
